dca1ef61746005d4c4098f595d270332eebd9867
Leonel Canario <leonelcnr@gmail.com>
2025-10-05 07:32:31 -0300

    commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import requests, io
+
+st.markdown("<h1 style='text-align: center'>🚀 PLUTONITA 🚀</h1><p style='text-align: center'>En busca de <strong>Exoplanetas</strong> 🪐</p>", unsafe_allow_html = True)
+
+dict_tables = {
+    "Seleccione una opcion": '',
+    "Kepler": "cumulative",
+    "Tess": "toi",
+    "Planetary Systems": "ps"
+}
+
+x = st.slider('Hyperparameter Tweaking', max_value=10)
+
+left_column, right_column = st.columns(2)
+option = st.selectbox("Dataset", dict_tables.keys())
+st.write(dict_tables[option])
+if option != "Seleccione una opcion":
+
+
+    LINK = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync"
+    q = f"SELECT top 10 * FROM {dict_tables[option]}"
+    r = requests.get(LINK, params={"query": q, "format": "csv"})
+
+    # @st.cache_data
+    def cargar_datos(nfilas):
+        data = pd.read_csv(
+            io.StringIO(r.text), 
+            nrows=nfilas
+        )
+        return data
+
+    data = cargar_datos(10)
+
+    st.write(data)
+
